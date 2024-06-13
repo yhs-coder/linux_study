633864d5b869db1daa0102198a73328b3c473409
yhs-coder <yhs6666@outlook.com>
2024-06-13 23:50:02 +0800

    获取进程退出的消息

--- process/test_wait.c
+++ process/test_wait.c
@@ -10,20 +10,38 @@ int main()
     if (id == 0)
     {
         //子进程
-        int cnt = 10;
+        int cnt = 5;
         while (cnt)
         {
             printf("我是子进程: %d, 父进程: %d cnt: %d\n",getpid(), getppid(),cnt--);
-            sleep(1);
+            sleep(1); 
+            
+            // 模拟进程异常退出的情况
+            
+            //int *p = NULL;
+            //*p = 10;
+            int a = 10;
+            a/=0;
         }
-        exit(0); // 子进程退出
+        
+        exit(12); // 子进程退出
     }
     
     // 父进程
-    sleep(15);
-    pid_t ret = wait(NULL);
+    //sleep(15);
+    //pid_t ret = wait(NULL);
+    
+    // 获取子进程退出的消息
+
+    int status = 0; // status是位图结构，低七位存储进程退出时发出的信号， 8-15位存储进程的退出码
+    pid_t ret = waitpid(id, &status, 0);
     if (id > 0)
-        printf("wait success: %d\n",ret);
+        printf("wait success: %d sig number: %d child exit code: %d\n",ret, (status & 0x7F), (status >> 8) & 0xFF);
     
     sleep(5);
+
+
+
+
+
 }
