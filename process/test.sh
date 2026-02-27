8b7fedcf9785650f55b6bb09f3aabf539e61859d
yhs-coder <yhs6666@outlook.com>
2026-02-27 23:36:59 +0800

    编写shell脚本，测试进程状态

--- process/test.sh
+++ process/test.sh
@@ -1,5 +1,6 @@
 while :; 
-do ps ajx |  head -1 && ps ajx | grep orphan_process | grep -v grep; 
-sleep 1;
-echo "----------------------------------------------------------------";
+do 
+	ps ajx | head -1 && ps ajx | grep -E 'zombie_process|orphan_process' | grep -v grep;
+	sleep 1;
+	echo "--------------------------------------------------------------------------";
 done
