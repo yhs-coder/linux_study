5af09864f32eaf71f929f5ae7f38d0c640a4948b
yhs-coder <yhs6666@outlook.com>
2026-04-06 23:58:40 +0800

    优化仿shell操作，使用内建命令，在父进程中直接执行 'cd切换目录以及echo打印内容'，其他命令再创建子进程子进程执行

--- process/mini_shell.c
+++ process/mini_shell.c
@@ -10,6 +10,8 @@
 #define OPT_NUM 64
 char line_command[NUM];
 char* argv[OPT_NUM];
+int last_code = 0;
+int last_sig = 0;
 
 int main() {
     while (1) {
@@ -23,12 +25,39 @@ int main() {
         assert(s != NULL);
         (void*)s;
         line_command[strlen(line_command) - 1] = 0;
-        printf("%s\n", line_command);
+        // printf("%s\n", line_command);
 
         // 字符串切割
         argv[0] = strtok(line_command, " ");
         int i = 1;
+
+        if (argv[0] != NULL & strcmp(argv[0], "ls") == 0) {
+            argv[i++] = (char*)"--color=auto";
+        }
+
+        // 如果没有子串， strtok->NULL, argv[end] = NULL
         while (argv[i++] = strtok(NULL, " "));
+
+        // 如果是cd命令，不需要创建子进程来执行，让shell自己执行对应的命令，本质是执行系统接口
+        // 像这种不需要创建子进程来执行而是让shell自己执行的命令 -- 内建/内置命令
+
+        // 像cd xxx/xxx 这种操作，直接切换到对应的文件路径
+        if (argv[0] != NULL && strcmp(argv[0], "cd") == 0) {
+            if (argv[1] != NULL) {
+                chdir(argv[1]);
+                continue;
+            }
+        }
+
+        // echo $? 或者 echo "hello xxx" 这种情况的处理
+        if (argv[0] != NULL && argv[1] != NULL && strcmp(argv[0], "echo") == 0) {
+            if (strcmp(argv[1], "$?") == 0) {
+                printf("%d, %d\n", last_code, last_sig);
+            } else {
+                printf("%s\n", argv[1]);
+            }
+            continue;
+        }
 
 #ifdef DEBUG
         for (int i = 0; argv[i]; i++) {
@@ -40,8 +69,12 @@ int main() {
         assert(pid != -1);
         if (pid == 0) {
             execvp(argv[0], argv);
-            exit(-1);
+            exit(1);
         }
-        waitpid(pid, NULL, 0);
+        int status = 0;
+        pid_t ret = waitpid(pid, &status, 0);
+        assert(ret > 0);
+        last_code = (status >> 8) & 0xFF;
+        last_sig = (status & 0x7F);
     }
 }
